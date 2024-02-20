11d31ea14c696438589bec9f35cd9d1583340bbd
Farzaanp <patwfk@farmingdale.edu>
2024-02-20 13:24:29 -0500

    Created the function to print hello world n times The printHelloWorld(n-1) is the recursive call Created the binary search function Created a low and high representing the index of the array Created the logic to compare the findNum to the respective half of the array Tested the function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -8,6 +10,17 @@ public class Main {
 
         printHelloWorld(n);
 
+        int[] arrNum = new int [7];
+        arrNum[0] = 3;
+        arrNum[1] = 6;
+        arrNum[2] = 7;
+        arrNum[3] = 9;
+        arrNum[4] = 13;
+        arrNum[5] = 23;
+        arrNum[6] = 25;
+
+        System.out.print(binarySearch(arrNum,21,0,7));
+
         }
     static void printHelloWorld(int n) {
         if (n > 0) {
@@ -15,5 +28,22 @@ public class Main {
             printHelloWorld(n - 1);
         }
     }
+    static int binarySearch(int[] arr, int findNum, int low, int high) {
+        if (low > high) {
+            return -1;  // base case so if target not found it returns -1
+        }
+
+        int mid = (high + low) / 2; // determines what the mid of the array is
+
+        if (arr[mid] == findNum) {
+            return mid;  // target found
+        } else if (arr[mid] > findNum) {
+            return binarySearch(arr, findNum, low, mid - 1);  // search in the left half, mid - 1 is the new high
+        } else {
+            return binarySearch(arr, findNum, mid + 1, high);  // search in the right half
+        }
+    }
+
+
 
 }
